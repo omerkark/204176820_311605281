9e989e48e398df643f3092dbd0d229614caab447
omerkark <omerkark@gmail.com>
2019-01-08 20:59:35 +0200

    config files

--- schedule.py
+++ schedule.py
@@ -1,2 +1,23 @@
 import sqlite3
+import os
+
+
+databaseexisted = os.path.isfile('schedule.db')
+
+dbConnection = sqlite3.connect('schedule.db')
+cursor = dbConnection.cursor()
+
+clock = 0;
+
+cursor.execute("SELECT * FROM courses")
+x = cursor.fetchall();
+
+# while(len(x) > 0):
+cursor.execute("SELECT * FROM classrooms as c JOIN courses ON c.id = courses.class_id WHERE current_course_time_left LIKE 0")
+freeClassRooms = cursor.fetchall()
+for i in freeClassRooms:
+    print(i)
+
+
+
 
